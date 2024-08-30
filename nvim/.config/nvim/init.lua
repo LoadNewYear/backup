0f918e62b1df45bc590e8d70f20e0f46fcfb1465
LoadNewYear <loadnewyear@gmail.com>
2024-08-30 11:38:02 +0530

    idk

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,14 @@
 require("config.lazy")
+require("config.treesitter")
+require("config.harpoon")
+require("config.undotree")
+require("config.fugitive")
+require("config.set")
+
 require("remap")
+require("remap.telescope")
+
+require("config.colors")
+
+vim.cmd.lua(ColorMyPencils("rose-pine"))
+require("config.harpoon")
--- nvim/.config/nvim/lua/remap/init.lua
+++ nvim/.config/nvim/lua/remap/init.lua
@@ -1,4 +1,17 @@
 vim.g.leader = " "
-vim.keymap.set("n", "<leader>pv",  vim.cmd.Ex )
-vim.keymap.set("n", "<leader>pf", function() require('telescope.builtin').find_files() end)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>n", vim.cmd.ZenMode)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d", "<C-d>zz")
+vim.keymap.set("n", "<C-U", "<C-u>zz")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
